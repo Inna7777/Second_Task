bc9a640016d82a161959fcfde138074250717bd9
Inna7777 <peshkova-inna@mail.ru>
2022-12-05 16:42:10 +0400

    Add the new task

---
+++ Task01/Program.cs
@@ -0,0 +1,8 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.*/
+Console.WriteLine("Введите трехзначное число ");
+string nStr = Console.ReadLine() ??"";
+int n = Convert.ToInt32(nStr);
+n =Math.Abs(n);
+int ntwo = n / 10 % 10;
+     Console.WriteLine ( ntwo+ " второе числo");
