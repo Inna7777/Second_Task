ae9df1cc7b68745b4de735006ea8a17f37b8f1ce
Inna7777 <peshkova-inna@mail.ru>
2022-12-05 16:58:14 +0400

    Add two programs

---
+++ Task02/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.*/
+Console.WriteLine("Введите число ");
+string nStr = Console.ReadLine() ??"";
+string [] mas = nStr.Select(i => i.ToString()).ToArray();
+
+ if ( nStr.Length < 3 )
+     {
+        Console.WriteLine("В веденном числе третьей цифры нет");
+      }
+   else
+   {
+    Console.WriteLine(mas[2]);
+   }          
---
+++ Task03/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.*/
+Console.WriteLine("Введите число от 1 до 7 ");
+int nday = Convert.ToInt32(Console.ReadLine());
+
+if (nday >7)
+    Console.WriteLine($"Нет такого дня недели");
+    else
+{
+  if (nday == 6 || nday == 7)
+  { 
+    if (nday == 6)
+    {
+    Console.WriteLine($"День {nday} (суббота)- является выводным днем ");
+    }
+    else
+    {
+    Console.WriteLine($"День {nday} (воскресенье)- является выводным днем ");
+    }
+   } 
+ else
+   {
+    Console.WriteLine($"День {nday} не является выводным днем");
+   }
+}
